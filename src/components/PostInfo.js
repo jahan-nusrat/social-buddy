4417bceafb1907f5e0afd43a7293e1bb517d05cf
Nusrat Jahan <njnusrat53@gmail.com>
2020-09-06 11:21:00 +0600

    ui design modify

--- src/components/PostInfo.js
+++ src/components/PostInfo.js
@@ -3,6 +3,28 @@ import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import Comments from './comments/Comments';
 import Loading from './Loading';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+	root  : {
+		maxWidth : 600
+	},
+	media : {
+		height       : 100,
+		width        : 100,
+		borderRadius : 50,
+		position     : 'absolute',
+		left         : '50%',
+		transform    : 'translateX(-50%)'
+	}
+});
 
 function PostInfo () {
 	const { id } = useParams();
@@ -10,6 +32,7 @@ function PostInfo () {
 	const [ comment, setComment ] = useState([]);
 	const [ loading, setLoading ] = useState(true);
 
+	const classes = useStyles();
 	const [ image, setImage ] = useState('');
 
 	useEffect(() => {
@@ -33,10 +56,31 @@ function PostInfo () {
 	}, []);
 
 	return (
-		<div>
-			<img src={image.large} alt="" />
-			<h2>{user.title}</h2>
-			<h5>{user.body}</h5>
+		<div className="container mt-4">
+			<div className="row justify-content-center">
+				<Card className={classes.root}>
+					<CardActionArea>
+						<CardMedia className={classes.media} image={image.medium} title="Contemplative Reptile" />
+						<CardContent style={{ marginTop: '6rem' }}>
+							<Typography gutterBottom variant="h5" component="h2">
+								{user.title}
+							</Typography>
+							<Typography variant="body2" color="textSecondary" component="p">
+								{user.body}
+							</Typography>
+						</CardContent>
+					</CardActionArea>
+					<CardActions>
+						<Button size="small" color="primary">
+							Share
+						</Button>
+						<Button size="small" color="primary">
+							Learn More
+						</Button>
+					</CardActions>
+				</Card>
+			</div>
+
 			<p>comments: {comment.length}</p>
 			{loading ? (
 				<Loading />
--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,22 +1,72 @@
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { Avatar } from '@material-ui/core';
+import { Avatar, List } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+	root   : {
+		width           : '100%',
+		maxWidth        : '36ch',
+		backgroundColor : theme.palette.background.paper
+	},
+	inline : {
+		display : 'inline'
+	}
+}));
 
 function Comments ({ item }) {
-	const [ image, setImage ] = useState('');
-
+	const [ image, setImage ] = useState({
+		name    : '',
+		picture : '',
+		email   : ''
+	});
+	const classes = useStyles();
 	useEffect(() => {
 		fetch('https://randomuser.me/api/').then((res) => res.json()).then((data) => {
-			setImage(data.results[0].picture);
+			setImage({
+				name    : data.results[0].name,
+				picture : data.results[0].picture,
+				email   : data.results[0].email
+			});
 		});
 	}, []);
 
+	console.log(image);
+
 	return (
-		<div>
-			<p>{item.body}</p>
-			<p>{item.email}</p>
-			<Avatar alt="Remy Sharp" src={image.medium} />
+		<div className="container">
+			<div className="row justify-content-center">
+				<List className="col-md-6 pt-0" style={{ backgroundColor: '#fff' }}>
+					<Divider variant="outset" component="li" />
+					<ListItem alignItems="flex-start">
+						<ListItemAvatar>
+							<Avatar alt="Cindy Baker" src={image.picture.medium} />
+						</ListItemAvatar>
+						<ListItemText
+							primary={image.name.title + ' ' + image.name.first + ' ' + image.name.last}
+							secondary={
+								<React.Fragment>
+									<Typography
+										component="span"
+										variant="body2"
+										className={classes.inline}
+										color="textPrimary"
+									>
+										{image.email}
+									</Typography>
+									<p>{item.body}</p>
+								</React.Fragment>
+							}
+						/>
+					</ListItem>
+				</List>
+			</div>
 		</div>
 	);
 }
